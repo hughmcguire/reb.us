fe8603ca3949dd3d67b3707062b6e5c6523e05a6
Boris Anthony <boris@anthony.net>
2016-02-01 17:46:45 -0500

    Initial commit

---
+++ bits/contact_us.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  Contact Us section
+*/
+?>
+<div class="section">
+  <h3>Get In Touch</h3>
+ <div class="expanded row">
+    <div class="medium-6 columns">
+      <form id="tinyletter" style="padding:0 20px 0 0; margin-right:20px;" action="https://tinyletter.com/rebusopentextbooks" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/rebusopentextbooks', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+        <p><label for="tlemail">Enter your email address to sign up to our Newsletter:</label></p>
+        <p>
+          <input type="text" name="email" id="tlemail" />
+          <input type="hidden" value="1" name="embed"/>
+          <input type="submit" value="Subscribe" />
+        </p>
+        <!--p><a href="https://tinyletter.com" target="_blank">powered by TinyLetter</a></p-->
+      </form>
+    </div>
+    <div class="medium-6 columns">
+      <p>
+        <a class="badger" href="https://twitter.com/rebus_ot/" title="Rebus Open Textbooks on Twitter"><img src="/img/badges/twitter.png" alt="Twitter"></a>
+        <a class="badger" href="https://medium.com/rebus-open-textbooks/" title="Rebus Open Textbooks on Medium"><img src="/img/badges/medium.png" alt="Medium"></a>
+        <a class="badger" href="https://github.com/RebusOpenTextbooks" title="Rebus Open Textbooks on GitHub"><img src="/img/badges/github.png" alt="GitHub"></a>
+        <a class="badger" href="https://www.facebook.com/RebusOpenTextbooks/" title=""><img src="/img/badges/facebook.png" alt="Rebus Open Textbooks on Facebook"></a>
+      </p>
+      <p><strong><a href="mailto:opentextbooks@reb.us?subject=Hi!">opentextbooks@reb.us</a></strong></p>
+    </div>
+  </div>
+</div>
---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Rebus Open Web Textbooks</title>
+  <link rel="apple-touch-icon" sizes="57x57" href="/favico/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/favico/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/favico/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/favico/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/favico/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/favico/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/favico/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/favico/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/favico/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/favico/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favico/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/favico/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favico/favicon-16x16.png">
+  <link rel="manifest" href="/favico/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/favico/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+<link href='https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700' rel='stylesheet' type='text/css'>
+  <link rel='stylesheet' type='text/css' href='/css/foundation.css'  >
+  <link rel='stylesheet' type='text/css' href='/css/styles.css'      >
+
+</head>
+<body>
+<header>
+  <h1>
+    <strong>Rebus</strong>
+    <em>Open Web Textbooks</em>
+  </h1>
+  <h2>Building&nbsp;knowledge to&nbsp;build&nbsp;on</h2>
+</header>
+<div class="container">
+
+  <div class="section">
+    <h3>What are we doing?</h3>
+    <p>Together with universities, professors, teachers, students, and regular people, we make and distribute free, Open Textbooks.</p>
+    <p>
+      <a href="/opentextbooks/" title="Rebus Open Textbooks">More about Open Textbooks >></a>
+    </p>
+  </div>
+
+  <div class="section">
+    <h3>How are we doing this?</h3>
+    <p>Rebus Open Textbooks embrace the structure and expectations we have of books, and add the power of the Open Web, enabling people—and machines—to use and build on them in new and meaningful ways.</p>
+    <p>
+      <a href="/openwebbooks/" title="Rebus Open Web Books">More about Open Web Books >></a>
+    </p>
+  </div>
+
+  <div class="section who">
+    <h3>Who are we?</h3>
+    <div class="expanded row">
+      <div class="medium-6 columns">
+        <h4>Hugh McGuire</h4>
+        <img class="thumbnail" src="/img/profiles/hughmcguire.png" alt="Hugh McGuire" />
+        <p>
+          Hugh has been building web tools and communities that bring books onto the web for a decade. He is the founder of <a href="http://LibriVox.org" title="LibriVox">LibriVox</a>, <a href="http://pressbooks.com/" title="Pressbooks">Pressbooks</a>, and the co-editor of “<a href="http://www.amazon.com/Book-Futurists-Manifesto-Collection-Publishing/dp/1449305601" title="Book: A Furturist’s Manifesto — Essays from the bleeding edge of publishing">Book: A Furturist’s Manifesto</a>.”
+          <br>
+          You can find him @<a href="https://twitter.com/hughmcguire" title="Hugh On Twitter">hughmcguire</a>.
+        </p>
+      </div>
+      <div class="medium-6 columns">
+        <h4>Boris Anthony</h4>
+        <img class="thumbnail" src="/img/profiles/borisanthony.png" alt="Boris Anthony" />
+        <p>
+          Boris has been designing, architecting, building on the Web since 1995. An Experience Architect &amp; Strategic Design Advisor, in recent years he's worked with <a href="http://nokia.com" title="Nokia">Nokia</a>, <a href="http://here.com" title="HERE">HERE</a>, <a href="https://en.wikipedia.org/wiki/Dopplr" title="about Dopplr">Dopplr</a> and <a href="https://globalvoices.org" title="GlobalVoices">GlobalVoices</a>.
+          <br>
+          You can find him via <a href="http://borisanthony.net/" title="BorisAnthony.net">BorisAnthony.net</a>.
+        </p>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="section advisors">
+    <h3>Our Advisors</h3>
+    <p>
+      <strong><a href="https://twitter.com/acroll" title="Alistair Croll on Twitter">Alistair&nbsp;Croll</a></strong><span class="comma">, </span>
+        Chair,
+        <em>O’Reilly&nbsp;Strata&nbsp;&amp; Next:Money&nbsp;conferences</em>
+      <br>
+      <strong><a href="http://jambina.com/" title="Amy Buckland on Twitter">Amy&nbsp;Buckland</a></strong><span class="comma">, </span>
+        Scholarly&nbsp;Communications,
+        <em>U&nbsp;Chicago&nbsp;Library</em>
+      <br>
+      <strong><a href="https://twitter.com/blaine" title="Blaine Cook on Twitter">Blaine&nbsp;Cook</a></strong><span class="comma">, </span>
+        CTO / Co-founder,
+        <em>Poetica</em>
+      <br>
+      <strong><a href="https://en.wikipedia.org/wiki/Brewster_Kahle" title="Brewster Kahle">Brewster&nbsp;Kahle</a></strong><span class="comma">, </span>
+        Director,
+        <em>Internet&nbsp;Archive</em>
+      <br>
+      <strong><a href="https://twitter.com/naypinya" title="Peter Brantley on Twitter">Peter&nbsp;Brantley</a></strong><span class="comma">, </span>
+        Director, Online&nbsp;Strategy,
+        <em>UC&nbsp;Davis&nbsp;Library</em>
+    </p>
+  </div>
+
+  <?php include 'bits/contact_us.php'; ?>
+
+  </div>
+
+</body>
+</html>
